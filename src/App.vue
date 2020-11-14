46261b303a7872df6ab6da6740bc3d010fb85629
yuriko192 <ivanwijaya19@yahoo.co.id>
2020-11-14 22:53:47 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,37 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <div class="header"></div>
     <router-view/>
+    <div class="footer">
+      Icons made by
+      <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a>,
+      <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a>,
+      <a href="https://www.flaticon.com/authors/prettycons" title="prettycons">prettycons</a>,
+      <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a>
+      from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+    </div>
   </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  @extend .flex-col;
+  margin: 0;
+  position: relative;
 }
 
-#nav {
-  padding: 30px;
+.header {
+  margin: 0;
+  padding: 10px 5px;
+  display: none;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.footer {
+  margin: 0;
+  padding: 10px 15px;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: $bg-second;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,92 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="https://avatars2.githubusercontent.com/u/30367416?s=460&u=237d28568e566fc4f387769974135345aa45eabc&v=4" alt="" class="profileImage">
+    <h1 class="name">Ivan Wijaya</h1>
+    <h3 class="jobdesc">Computer Science Student<br/>Tarumanagara University</h3>
+    <div class="areaofinterest">
+      <h2 class="title">Area Of Interest</h2>
+      <div class="item">
+        <h4>
+          <span><img src="@/assets/fullstack.svg" alt="" class="icon-text"></span>
+          Fullstack Developer
+        </h4>
+      </div>
+      <div class="item">
+        <h4>
+          <span><img src="@/assets/UIDesign.svg" alt="" class="icon-text"></span>
+          UI Designer
+        </h4>
+      </div>
+      <div class="item">
+        <h4>
+          <span><img src="@/assets/Mobile.svg" alt="" class="icon-text"></span>
+          Mobile Programmer
+        </h4>
+      </div>
+      <div class="item">
+        <h4>
+          <span><img src="@/assets/AI.svg" alt="" class="icon-text"></span>
+          AI Engineer
+        </h4>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.home{
+  @extend .flex-col;
+  align-items: center;
+  margin: 0;
+  position: relative;
+  background-color: $bg-col;
+  &:before{
+    content: '';
+    z-index: 0;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background-image: linear-gradient(rgba(0,0,0,0), $bg-col 60%),url("../assets/BG.jpg");
+    background-repeat: no-repeat;
+    background-position: top;
+    background-size: cover;
+  }
+  .profileImage{
+    margin-top: 25vh;
+    $size: 300px;
+    width: $size;
+    border-radius: $size;
+  }
+  *{
+    z-index: 2;
+  }
+  .jobdesc{
+    text-align: center;
+  }
+  .areaofinterest{
+    @extend .flex-col;
+    margin-top: 50px;
+    background-color: $bg-second;
+    align-self: stretch;
+    padding: 15px 10px;
+    border-radius: 10px;
+    .title{
+      text-align: center;
+    }
+    .item{
+      width: 100%;
+    }
+  }
+}
+</style>
