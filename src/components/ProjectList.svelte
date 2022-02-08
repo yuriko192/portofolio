ae11e2312fc647c7b7af70a82b1d59f636713e81
yuriko192 <ivanwijaya19@yahoo.co.id>
2022-02-08 23:57:07 +0700

    init

---
+++ src/components/ProjectList.svelte
@@ -0,0 +1,15 @@
+<script>
+    let name = 'world';
+</script>
+
+<h1>Hello {name}!</h1>
+
+<h1 class="font-bold text-2xl">Featured Projects</h1>
+<div class="subJob my-4 grid gap-4 grid-flow-col grid-flow-row-dense sm:grid-flow-col-dense grid-cols-4 grid-rows-2 lg:grid-rows-1">
+    <div class="col-span-4 sm:col-span-2 lg:col-span-1">Funcraft Web Application</div>
+    <div class="col-span-4 sm:col-span-2 lg:col-span-1">Confins Core Banking System</div>
+    <div class="col-span-4 sm:col-span-2 lg:col-span-1">DeText OCR</div>
+    <div class="col-span-4 sm:col-span-2 lg:col-span-1">Hotel Reservation System</div>
+    <div class="col-span-4 sm:col-span-2 lg:col-span-1">Speedwagon Email Client</div>
+    <div class="col-span-4 sm:col-span-2 lg:col-span-1">Redesign M Banking BCA</div>
+</div>
