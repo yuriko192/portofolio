7822b0394689cf5749edef9b084f879e9754ae41
yuriko192 <ivanwijaya19@yahoo.co.id>
2022-02-09 21:46:28 +0700

    init

--- src/components/ProjectList.svelte
+++ src/components/ProjectList.svelte
@@ -1,15 +1,36 @@
 <script>
-    let name = 'world';
+    import * as ProjectJson from '../Data/Projects.json'
 </script>
+<style lang="scss">
+  .BGImg {
+    z-index: -2;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background-repeat: no-repeat;
+    background-position: top;
+    background-size: cover;
+  }
+</style>
 
-<h1>Hello {name}!</h1>
+<h1 class="font-bold text-2xl">Featured Projects </h1>
+<div class="my-4 grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+    {#each ProjectJson['projects'] as project}
+        <div class="relative h-[25vh] pt-[20%]">
+            <div class="absolute BGImg"
+                 style="{'background-image: linear-gradient(rgba(MidnightBlue, 0.2), rgba(MidnightBlue, 0.2) 100%), url('+project.image+');'}"></div>
+            <h1 class="absolute text-3xl font-bold w-full text-center align-middle text-white">{project.title}</h1>
+            <div class="absolute bg-gray-900 bottom-0 w-full h-1/3 p-3">
+                <p class="text-white">{project.desc}</p>
+            </div>
+        </div>
+    {/each}
 
-<h1 class="font-bold text-2xl">Featured Projects</h1>
-<div class="subJob my-4 grid gap-4 grid-flow-col grid-flow-row-dense sm:grid-flow-col-dense grid-cols-4 grid-rows-2 lg:grid-rows-1">
-    <div class="col-span-4 sm:col-span-2 lg:col-span-1">Funcraft Web Application</div>
-    <div class="col-span-4 sm:col-span-2 lg:col-span-1">Confins Core Banking System</div>
-    <div class="col-span-4 sm:col-span-2 lg:col-span-1">DeText OCR</div>
-    <div class="col-span-4 sm:col-span-2 lg:col-span-1">Hotel Reservation System</div>
-    <div class="col-span-4 sm:col-span-2 lg:col-span-1">Speedwagon Email Client</div>
-    <div class="col-span-4 sm:col-span-2 lg:col-span-1">Redesign M Banking BCA</div>
+    <div class="">Confins Core Banking System</div>
+    <div class="">DeText OCR</div>
+    <div class="">Hotel Reservation System</div>
+    <div class="">Speedwagon Email Client</div>
+    <div class="">Redesign M Banking BCA</div>
 </div>
