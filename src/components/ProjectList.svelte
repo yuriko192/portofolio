fe8311e371a8ab28198a9a1757e7edfea52340b1
yuriko192 <ivanwijaya19@yahoo.co.id>
2022-02-09 23:09:05 +0700

    0.1 - main content completed

--- src/components/ProjectList.svelte
+++ src/components/ProjectList.svelte
@@ -16,21 +16,15 @@
 </style>
 
 <h1 class="font-bold text-2xl">Featured Projects </h1>
-<div class="my-4 grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+<div class="my-4 grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
     {#each ProjectJson['projects'] as project}
-        <div class="relative h-[25vh] pt-[20%]">
+        <div class="relative h-[25vh] pt-[10%]">
             <div class="absolute BGImg"
-                 style="{'background-image: linear-gradient(rgba(MidnightBlue, 0.2), rgba(MidnightBlue, 0.2) 100%), url('+project.image+');'}"></div>
+                 style="{'background-image: linear-gradient(rgba(0,0,0, 0.5), rgba(0,0,0, 0.5) 100%), url('+project.image+');'}"></div>
             <h1 class="absolute text-3xl font-bold w-full text-center align-middle text-white">{project.title}</h1>
             <div class="absolute bg-gray-900 bottom-0 w-full h-1/3 p-3">
                 <p class="text-white">{project.desc}</p>
             </div>
         </div>
     {/each}
-
-    <div class="">Confins Core Banking System</div>
-    <div class="">DeText OCR</div>
-    <div class="">Hotel Reservation System</div>
-    <div class="">Speedwagon Email Client</div>
-    <div class="">Redesign M Banking BCA</div>
 </div>
