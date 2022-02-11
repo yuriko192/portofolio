9f3cdc385eebd37511fd258f8679a42a94388a10
yuriko192 <ivanwijaya19@yahoo.co.id>
2022-02-11 23:33:57 +0700

    1.0 - Initial Release

--- src/components/WorkList.svelte
+++ src/components/WorkList.svelte
@@ -1,5 +1,5 @@
 <script>
-    import * as ProjectJson from '../Data/Projects.json'
+    import * as WorkJson from '../Data/Works.json'
 </script>
 <style lang="scss">
   .BGImg {
@@ -15,15 +15,15 @@
   }
 </style>
 
-<h1 class="font-bold text-2xl">Featured Projects </h1>
+<h1 class="font-bold text-2xl">Past Work Experience </h1>
 <div class="my-4 grid gap-4" style="grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr))">
-    {#each ProjectJson['projects'] as project}
+    {#each WorkJson['jobs'] as job}
         <div class="relative h-64 pt-[10%]">
             <div class="absolute BGImg"
-                 style="{'background-image: linear-gradient(rgba(0,0,0, 0.5), rgba(0,0,0, 0.5) 100%), url('+project.image+');'}"></div>
-            <h1 class="absolute text-3xl font-bold w-full text-center align-middle text-white">{project.title}</h1>
+                 style="{'background-image: linear-gradient(rgba(0,0,0, 0.5), rgba(0,0,0, 0.5) 100%), url('+job.image+');'}"></div>
+            <h1 class="absolute text-3xl font-bold w-full text-center align-middle text-white">{job.title}</h1>
             <div class="absolute bg-gray-900 bottom-0 w-full h-2/5 p-3">
-                <p class="text-white">{project.desc}</p>
+                <p class="text-white">{job.desc}</p>
             </div>
         </div>
     {/each}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,13 @@ module.exports = {
         './public/**/*.html',
         './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}',
     ],
+    theme: {
+        extend: {
+            screens: {
+                'tall': { 'raw': '(min-height: 800px)' },
+                'xtall': { 'raw': '(min-height: 1000px)' },
+            }
+        }
+    }
     // more options here
 };
