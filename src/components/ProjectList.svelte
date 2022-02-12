6a526788bbe6f1ee158f60f3b8b1c8719bfd3b0d
yuriko192 <ivanwijaya19@yahoo.co.id>
2022-02-12 23:11:42 +0700

    1.0.1 - added project modal

--- src/components/ProjectList.svelte
+++ src/components/ProjectList.svelte
@@ -1,5 +1,13 @@
 <script>
     import * as ProjectJson from '../Data/Projects.json'
+
+    let isShowDetail = false;
+    let currProject = {};
+
+    function showDetail(Cond, Project) {
+        isShowDetail = Cond;
+        currProject = Project;
+    }
 </script>
 <style lang="scss">
   .BGImg {
@@ -13,18 +21,50 @@
     background-position: top;
     background-size: cover;
   }
+
+  .dark-BG:hover {
+    background-color: rgba(black, 0.5);
+
+    * {
+      @apply visible translate-y-0
+    }
+  }
+
+  .modalCont {
+    background-color: rgba(black, 0.7);
+  }
 </style>
 
 <h1 class="font-bold text-2xl">Featured Projects </h1>
 <div class="my-4 grid gap-4" style="grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr))">
     {#each ProjectJson['projects'] as project}
-        <div class="relative h-64 pt-[10%]">
-            <div class="absolute BGImg"
-                 style="{'background-image: linear-gradient(rgba(0,0,0, 0.5), rgba(0,0,0, 0.5) 100%), url('+project.image+');'}"></div>
-            <h1 class="absolute text-3xl font-bold w-full text-center align-middle text-white">{project.title}</h1>
-            <div class="absolute bg-gray-900 bottom-0 w-full h-2/5 p-3">
-                <p class="text-white">{project.desc}</p>
+        <div class="relative overflow-hidden h-64 rounded drop-shadow-lg">
+            <div>
+                <div class="absolute BGImg"
+                     style="{'background-image: linear-gradient(rgba(0,0,0, 0.5), rgba(0,0,0, 0.5) 100%), url('+project.image+');'}"></div>
+                <h1 class="mt-[10%] absolute text-3xl font-bold w-full text-center align-middle text-white">{project.title}</h1>
+                <div class="absolute bg-gray-900 bottom-0 w-full h-2/5 p-3">
+                    <p class="text-white">{project.desc}</p>
+                </div>
+            </div>
+            <div class="absolute w-full h-full dark-BG grid transition" on:click={()=>{showDetail(true, project)}}>
+                <div class="place-self-center overflow-hidden p-1">
+                    <button class="invisible rounded-lg py-2 px-4 hover:bg-slate-600 active:bg-gray-100 text-gray-100 active:text-slate-600 outline outline-2 outline-gray-100 active:outline-slate-600 translate-y-10 transition-all">
+                        More Details
+                    </button>
+                </div>
             </div>
         </div>
     {/each}
+</div>
+<div class="fixed top-0 left-0 h-screen w-screen z-50 modalCont grid backdrop-blur p-4 {isShowDetail? 'block':'hidden'}"
+     on:click={(e)=>{if(e.target===e.currentTarget)showDetail(false, currProject)}}>
+    <div class="relative place-self-center w-full max-w-[47.375rem] h-full max-h-[84rem] bg-gray-50 rounded shadow z-50">
+        <div class="absolute BGImg"
+             style="{'background-image: linear-gradient(rgba(0,0,0, 0.5), rgba(0,0,0, 0.5) 100%), url('+currProject.image+');'}"></div>
+        <h1 class="mt-[10%] absolute text-3xl font-bold w-full text-center align-middle text-white">{currProject.title}</h1>
+        <div class="absolute bg-gray-900 bottom-0 w-full h-2/5 p-3">
+            <p class="text-white">{currProject.desc}</p>
+        </div>
+    </div>
 </div>
